7aaab0206804f2dbb765da5d11579c789f39dcdd
james.marvin <james.marvin@naimuri.com>
2024-01-12 11:13:04 +0000

    Move searchmap logic from Point to SearchMap

--- src/geometry/point.py
+++ src/geometry/point.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 from typing import List
 import math
 
-from geometry.constants import SEARCH_MAP_SIZE, SEARCH_RAD
-
 class Point: 
     def __init__(self, x: int, y: int):
         self._x = x
@@ -32,12 +30,6 @@ class Point:
     
     def scaled(self, scale_factor: float):
         return Point(self.x * scale_factor, self.y * scale_factor)
-
-    @staticmethod
-    def inverse_coordinate(coordinate: int) -> int: 
-        """Inverse a coordinate value as per a bottom-right-oriented grid."""
-        inversed = SEARCH_MAP_SIZE - coordinate
-        return inversed - 1
         
     @property
     def x(self) -> int: 
@@ -54,48 +46,6 @@ class Point:
         """Return the distance to the origin (0, 0). Useful for sorting."""
         return self.distance_to(Point(0,0))
     
-    @property
-    def inverse_x(self) -> int:
-        return Point.inverse_coordinate(self.x)
-    
-    @property
-    def inverse_y(self) -> int: 
-        return Point.inverse_coordinate(self.y)
-
-    @property
-    def search_min_x(self) -> int: 
-        """Return the left-most X coordinate for the point's bounding box."""
-        min_x = max(0, math.floor(self.x - SEARCH_RAD))
-        return min_x
-    
-    @property
-    def search_max_x(self) -> int:
-        """Return the right-most X coordinate for the point's bounding box."""
-        max_x = min(SEARCH_MAP_SIZE, math.ceil(self.x + SEARCH_RAD))
-        return max_x
-    
-    @property
-    def search_min_y(self) -> int:
-        """Return the bottom-most Y coordinate for the point's bounding box."""
-        min_y = max(0, math.floor(self.y - SEARCH_RAD))
-        return min_y
-    
-    @property
-    def search_max_y(self) -> int: 
-        """Return the top-most Y coordinate for the point's bounding box."""
-        max_y = min(SEARCH_MAP_SIZE, math.ceil(self.y + SEARCH_RAD))
-        return max_y
-    
-    @property
-    def binary_grid_index_position(self) -> int: 
-        """Return the grid index position for the point."""
-        return (SEARCH_MAP_SIZE * self.inverse_y) + self.inverse_x
-    
-    @property
-    def binary_grid_value(self) -> int: 
-        """Derive a binary number from the point's grid index position."""
-        return 2**self.binary_grid_index_position
-    
     def distance_to(self, point: Point) -> float: 
         """Calculate the distance to another point."""
         x_delta = self.x - point.x
@@ -103,21 +53,9 @@ class Point:
         distance_to_point = math.hypot(x_delta, y_delta)
         return distance_to_point
     
-    def get_points_in_search_radius(self) -> List[Point]:
-        """List of points within the current point's search radius."""
-        points_in_search_radius = []
-
-        for x_value in range(self.search_min_x, self.search_max_x): 
-            for y_value in range(self.search_min_y, self.search_max_y):
-                point = Point(x_value, y_value)
-                if self.distance_to(point) <= SEARCH_RAD: 
-                    points_in_search_radius.append(point)
-
-        return points_in_search_radius
-    
-    @property
-    def is_in_bounds(self) -> bool: 
-        """Indicate whether the specified point is in bounds of the map."""
-        in_x = 0 <= self.x <= SEARCH_MAP_SIZE
-        in_y = 0 <= self.y <= SEARCH_MAP_SIZE
-        return in_x and in_y
+    # @property
+    # def is_in_bounds(self) -> bool: 
+    #     """Indicate whether the specified point is in bounds of the map."""
+    #     in_x = 0 <= self.x <= SEARCH_MAP_SIZE
+    #     in_y = 0 <= self.y <= SEARCH_MAP_SIZE
+    #     return in_x and in_y
--- src/map/searchmap.py
+++ src/map/searchmap.py
@@ -1,27 +1,115 @@
-from point import Point
-import constants
+from typing import List
+import math
+from functools import cached_property
+
+from geometry.point import Point
 
 class SearchMap: 
-    def __init__(self, map: int=0):
-        self._map = map
+    def __init__(
+        self, 
+        map_width_px: int,
+        map_height_px: int,
+        search_radius: float, 
+        map: int=0
+    ):
+        # TODO: make map private, getters / setters etc
+        self.map = map
+        self.map_width_px = map_width_px
+        self.map_height_px = map_height_px
+        self.search_radius = search_radius
 
-    def __str__(self) -> str: 
+    @cached_property
+    def n_bits(self):
+        return self.map_height_px * self.map_width_px
+
+    def __str__(self) -> str:
+        """Iterate through the string map to print to console."""
+        input = self.__as_str()
+        output = ""
+        
+        for row in range(0, self.map_height_px): 
+            min = row * self.map_width_px
+            max = min + self.map_width_px
+            output += input[min:max] + "\n"
+
+        return output
+
+    def __as_str(self) -> str: 
         """Return a string representation of the map."""
-        str_map_val = str(bin(self._map))[2:]
-        return str_map_val.rjust(constants.SEARCH_MAP_SIZE**2, "0")
+        str_map_val = str(bin(self.map))[2:]
+        return str_map_val.rjust(self.n_bits, "0")
+
+    def __max_search_val(self) -> int:
+        return (1 << (self.n_bits)) -1
 
     def reveal(self, point: Point) -> None: 
         """Alter to map to reveal the 'tile' at the given Point position."""
-        self._map |= point.binary_grid_value
+        self.map |= self.point_binary_grid_value(point)
 
-    def reveal_radius(self, point: Point, radius: float = 0.0) -> None:
+    def reveal_radius(self, point: Point) -> None:
         """Reveal a radius around a point."""
-        for given_point in point.get_points_in_search_radius(): 
+        for given_point in self.get_points_in_search_radius(
+            centre=point
+        ): 
             self.reveal(given_point)
 
-    def show(self) -> None:
-        """Iterate through the string map to print to console."""
-        for row in range(0, constants.SEARCH_MAP_SIZE): 
-            min = row * constants.SEARCH_MAP_SIZE
-            max = min + constants.SEARCH_MAP_SIZE
-            print(self.str_map[min:max])
+    def __inverse_coordinate(self, coordinate: int, span: int) -> int:
+        """Inverse a coordinate value as per a bottom-right-oriented grid."""
+        return (span - coordinate) -1
+
+    def __inverse_point_x(self, point: Point) -> int:
+        return self.__inverse_coordinate(point.x, self.map_width_px)
+
+    def __inverse_point_y(self, point: Point) -> int:
+        return self.__inverse_coordinate(point.y, self.map_height_px)
+
+    def __inverse_point(self, point: Point) -> int:
+        return Point(
+            self.__inverse_point_x(point), self.__inverse_point_y(point)
+        )
+
+    def point_binary_grid_index_position(self, point: Point) -> int:
+        inversed = self.__inverse_point(point)
+
+        return (
+            (self.map_width_px * inversed.y) +
+            inversed.x
+        )
+
+    def point_binary_grid_value(self, point: Point) -> int:
+        return 2**self.point_binary_grid_index_position(point)
+
+    def point_search_min_x(self, point: Point) -> int:
+        return max(0, math.floor(point.x - self.search_radius))
+
+    def point_search_max_x(self, point: Point) -> int:
+        return min(self.map_width_px, math.ceil(point.x + self.search_radius))
+
+    def point_search_min_y(self, point: Point) -> int:
+        return max(0, math.floor(point.y - self.search_radius))
+
+    def point_search_max_y(self, point: Point) -> int:
+        return min(self.map_height_px, math.ceil(point.y + self.search_radius))
+
+    def get_points_in_search_radius(
+        self, 
+        centre: Point, 
+        search_radius: float = None
+    ) -> List[Point]:
+        """List of points within search radius of a given point."""
+        if not search_radius:
+            search_radius = self.search_radius
+
+        points_in_search_radius = []
+
+        for x_value in range(
+            self.point_search_min_x(centre), self.point_search_max_x(centre)
+        ): 
+            for y_value in range(
+                self.point_search_min_y(centre), self.point_search_max_y(centre)
+            ):
+                point = Point(x_value, y_value)
+                if centre.distance_to(point) <= search_radius: 
+                    points_in_search_radius.append(point)
+
+        return points_in_search_radius
