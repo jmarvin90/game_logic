82a7dcd7fe86403d466cfdd30bc479b1157c418c
james.marvin <james.marvin@naimuri.com>
2024-01-22 09:50:59 +0000

    Map tests

---
+++ src/tests/map/test_search_map.py
@@ -0,0 +1,25 @@
+import pytest
+
+from map.searchmap.searchmap import SearchMap
+from geometry.point import Point
+from geometry.edge import Edge
+
+@pytest.fixture
+def test_search_map() -> SearchMap:
+    return SearchMap(
+        map=0, 
+        map_height_px=32, 
+        map_width_px=128, 
+        search_radius_px=7.5,
+        scale_factor=1
+    )
+
+def test_print_search_map(test_search_map: SearchMap):
+    # test_search_map.reveal_radius(Point(0, 0))
+    # test_search_map.reveal_radius(Point(512, 64))
+    test_search_map.reveal_radius(Point(0, 0))
+    # test_search_map.reveal(Point(0, 0))
+    # test_search_map.reveal(Point(64, 64))
+    # test_search_map.reveal(Point(127, 127))
+    print(test_search_map)
+    assert False
